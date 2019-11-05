1002f56edc70059334abad5a74c9428ea919acb0
TanTan Wang <tantan.wang@yale.edu>
2019-11-04 23:44:14 -0500

    photos and readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# tantan-knochbox
-Puzzle Box with Motors! Module 4/5 Task 2
+# Introducing: The KnöchBox, a unnecessarily named Puzzle Box
+
+![alt text][intro]
+
+[intro]: https://github.com/tantantheman/tantan-whoopee/blob/master/documentation_photos/cushionslant.jpg "Wireless Whoopee Wonder"
+
+A puzzle box for class. Module 4/5, Task 2 (Kinetic Art).  
+Programming in Arduino IDE.  
+
+**Dependencies:**  
+ESP32Servo.h  
+Stepper.h
+
+**How to play:**  
+
+Objective: The goal in the box is to try and guess the correct sequence of knocks in order to "unlock" the puzzle box. If the number of knocks in a set is correct, you advance onto the next section of the 5-set sequence. An incorrect guess will reveal how close you were to guessing that set, allowing you to try again. When the box is unlocked, it resets. 
+
+**How to run:**  
+Plug in ESP32 inside the KnöchBox by opening up the top lid, and extending cable out through side panel with cutout. Enjoy!
+  
+**Implementation:**  
+To create the puzzle box that is the KnöchBox, only a few parts were actually needed.
+
+The enclosure was largely inspired by that of the Fart-O-Matic, where utilizing the top panel of the box essentially as a large button allows for the "knocking" effect that the user can use to interact with the box.
+
+Mechanics wise, there are three servo motors that act as guages, one that tells the "closeness" or accuracy of the knock attempt, one that tells the player how close they are to unlocking the box, and finally one guage that indicates the locked/unlocked status of the box.  
+
+There is also a button that is implemented in the same way as the Fart-O-Matic, allowing for the previously discussed top knocking panel.  
+
+**Technical Difficulties:**  
+The largest technical difficulty with building this project was actually figuring out a way to attach the motors in a way that worked with the thickness of the wooden enclosure. At the end, a combination of electric tape and shipping tape seemed to do the trick. In addition, calibrating the knocking to register successfully, and figuring out a timing to register an attempt was difficult, as it just relied on trial and error. 
+
+**Video**  
+A video of the KnöchBox in action can be found here:  
---
+++ knochbox/knochbox.ino
@@ -0,0 +1,151 @@
+#include <ESP32Servo.h>
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2038;
+const int incrementSteps = 407.6;
+Stepper myStepper(stepsPerRevolution, 32, 33, 25, 26);
+Servo myServo;
+Servo progressServo; 
+Servo lockServo;
+
+const int buttonPin = 4;
+const int dcPin = 24;
+const int thingOn = 1;
+const int thingOff = 0;
+
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+int numZeros = 0;
+int firstPress = 1;
+
+int numKnocks = 0;
+int sequenceSolved = 0;
+int readyToGo = 1;
+int placeInSequence = 0;
+
+int puzzleSequence[5] = {1, 2, 3, 4, 5};
+
+int servoPosition = 180;
+int stepperRotations = 0;
+
+void setup() {
+  myServo.attach(27);
+  progressServo.attach(25);
+  lockServo.attach(26);
+  Serial.begin(115200);
+//  myStepper.setSpeed(6);
+  myServo.write(servoPosition);
+  progressServo.write(180);
+  lockServo.write(144);  
+
+
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop(){
+  buttonState = digitalRead(buttonPin);
+  if (sequenceSolved == 1)
+  {
+    readyToGo = 0;
+    for (int i = 0; i < 5; i++){
+      puzzleSequence[i] = random(4, 10);
+      Serial.print("new random sequence = ");
+      Serial.println(puzzleSequence[i]);
+    }
+    sequenceSolved = 0;
+    readyToGo = 1;
+  }
+
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == LOW) {
+      // if the current state is HIGH then the button went from off to on:
+      buttonPushCounter++;
+      if (firstPress == 1)
+      {
+        numZeros = 0;
+        firstPress = 0;
+      }
+      else
+      {
+        numZeros = 0;
+      }
+      Serial.println(thingOn, DEC);
+    } else {
+      // if the current state is LOW then the button went from on to off:
+      Serial.println(thingOff, DEC);
+    } 
+   }
+   else 
+   {
+    numZeros++;
+    //Serial.println(thingOff, DEC); 
+    Serial.println(buttonPushCounter);
+   } 
+
+  
+  lastButtonState = buttonState;
+ 
+ if (numZeros > 8000 && firstPress != 1)
+ {
+
+ if (buttonPushCounter == puzzleSequence[placeInSequence])
+  {
+    buttonPushCounter = 0;
+    numZeros = 0;
+    firstPress = 1;
+    Serial.println("one place solved");
+    servoPosition = 180;
+    myServo.write(servoPosition); 
+    int progressPosition = map(placeInSequence, 0, 4, 180, 0);
+    progressServo.write(progressPosition);
+    
+    myStepper.step(incrementSteps);
+    if (placeInSequence == 4)
+    {
+      Serial.println("PUZZLE SOLVED");
+      placeInSequence = 0;
+      sequenceSolved = 1;
+      lockServo.write(36);
+      delay(4000);
+      lockServo.write(144);
+      progressServo.write(180);
+      
+    }
+    placeInSequence++;
+    
+  }
+
+ if (buttonPushCounter < puzzleSequence[placeInSequence])
+ {
+    float percentageVal = ((float) buttonPushCounter/puzzleSequence[placeInSequence])*100;
+    int percentToInt = (int) percentageVal;
+    servoPosition = map(percentToInt, 0, 100, 180, 0);
+    Serial.print("percentageVal is ");
+    Serial.println(percentageVal);
+    Serial.print("int version is");
+    Serial.println(percentToInt);
+    Serial.print("servoPosition is ");
+    Serial.println(servoPosition);
+    //delay(1000);
+    //percentage of accuracy mapped to servo range
+    myServo.write(servoPosition);  
+    numZeros = 0;
+    buttonPushCounter = 0;
+    firstPress = 1;
+ }
+ else 
+ {
+    servoPosition = 180;
+    myServo.write(servoPosition);
+    numZeros = 0;
+    buttonPushCounter = 0;
+    firstPress = 1;
+ } 
+ }
+
+
+  
+  
+}
