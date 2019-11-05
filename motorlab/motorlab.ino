2e44ae3c97e8c9fb4eac1baf6e3985dd95fd21d9
TanTan Wang <tantan.wang@yale.edu>
2019-11-04 19:23:45 -0500

    timing almost sorted

--- motorlab/motorlab.ino
+++ motorlab/motorlab.ino
@@ -1,5 +1,8 @@
 #include <ESP32Servo.h>
 #include <Stepper.h>
+#include <timer.h>
+
+auto timer = timer_create_default(); // create a timer with default settings
 
 const int stepsPerRevolution = 2038;
 const int incrementSteps = 407.6;
@@ -14,6 +17,8 @@ const int thingOff = 0;
 int buttonPushCounter = 0;   // counter for the number of button presses
 int buttonState = 0;         // current state of the button
 int lastButtonState = 0;     // previous state of the button
+int numZeros = 0;
+int firstPress = 1;
 
 int numKnocks = 0;
 int sequenceSolved = 0;
@@ -52,6 +57,11 @@ void loop(){
     if (buttonState == LOW) {
       // if the current state is HIGH then the button went from off to on:
       buttonPushCounter++;
+      if (firstPress == 1)
+      {
+        numZeros = 0;
+        firstPress = 0;
+      }
       Serial.println(thingOn, DEC);
     } else {
       // if the current state is LOW then the button went from on to off:
@@ -60,12 +70,16 @@ void loop(){
    }
    else 
    {
-    Serial.println(thingOff, DEC); 
+    numZeros++;
+    //Serial.println(thingOff, DEC); 
+    Serial.println(numZeros);
    } 
 
   if (buttonPushCounter == puzzleSequence[placeInSequence])
   {
     buttonPushCounter = 0;
+    numZeros = 0;
+    firstPress = 1;
     Serial.println("one place solved");
     myStepper.step(incrementSteps);
     if (placeInSequence == 4)
@@ -85,24 +99,32 @@ void loop(){
  // stepperRotations++;
  // servoPosition = (servoPosition + 10) % 180;
   
-/*
- if (numKnocks < puzzleSequence[placeInSequence])
+/* if (numZeros >4000 && firstPress != 1)
  {
- servoPosition = map(numKnocks, 0, 1.0, 0, 180);
- //percentage of accuracy mapped to servo range
- myServo.write(servoPosition);
- delay(100);
- servoPosition = 0;
- myServo.write(servoPosition);
+  Serial.println("NOWNOWNOWNOWNOWOWN");
+  delay(100);
+  numZeros = 0;
+ } */
+ if (numZeros > 5000 && firstPress != 1)
+ {
+ if (buttonPushCounter < puzzleSequence[placeInSequence])
+ {
+    servoPosition = map(buttonPushCounter, 0, 1.0, 0, 180);
+    //percentage of accuracy mapped to servo range
+    myServo.write(servoPosition);
+    delay(50);
+    servoPosition = 0;
+    myServo.write(servoPosition);
  }
  else 
  {
- servoPosition = 180;
- myServo.write(servoPosition);
- delay(100);
- servoPosition = 0;
- myServo.write(servoPosition);
- } */
+    servoPosition = 180;
+    myServo.write(servoPosition);
+    delay(50);
+    servoPosition = 0;
+    myServo.write(servoPosition);
+ } 
+ }
 
   // set motor positions
   //myStepper.step(stepsPerRevolution);
